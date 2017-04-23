39c6d7e05bead8778a18f8d5057d8aa5c2b6add8
austin imbastari <austin imbastari>
2017-04-22 20:35:24 -0700

    refactor possession of premises

--- src/agreement/PossessionOfPremises.js
+++ src/agreement/PossessionOfPremises.js
@@ -7,13 +7,15 @@ import { Field } from 'redux-form';
 const PossessionOfPremises = ({}) => {
   return (
     <Segment>
-      <Label>
-        In the event Landlord is unable to deliver possession of the premises to Resident for any reason not within
-      Landlords control, including, but not limited to, failure of prior occupants to vacate as agreed or required
-      by law, Landlord
-      </Label>
       <div className="fields">
-          <div className="three wide field">
+          <div className="sixteen wide field">
+            <label>
+            In the event Landlord is unable to deliver possession of the premises to Resident for any reason not within
+            Landlords control, including, but not limited to, failure of prior occupants to vacate as agreed or required
+            by law, Landlord shall not be liable to Residents except for the return of all sums previously paid to Landlord
+            in the event the Residents choose to terminate this Lease.
+            </label>
+            
             <div>
               <Label htmlFor="ackPossessionOfPremises">Click to Acknowledge</Label>
               <div>
