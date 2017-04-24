2ae80be403ac54557b70c4c83b8378c93013a847
Imbastari <t562731@IMSWEST.SSCIMS.COM>
2017-04-24 10:37:55 -0700

    add text

--- src/agreement/SecurityDeposit.js
+++ src/agreement/SecurityDeposit.js
@@ -7,31 +7,26 @@ import { Field } from 'redux-form';
 const SecurityDeposit = ({}) => {
   return (
     <Segment>
-      <Label>
-        Resident agrees to pay to Landlord, at the office of the apartment manager, monthly rental at the
-        rate set forth in paragraph
-          Monthly rent
-          Garage/Storage
-          on or before the FIRST (1st) day of each month.
-          Rent is payable only by personal check, cashiers check or money order payable to:
-          Romneya Apartment Management.
-          No cash will be accepted. In the event Landlord is required to serve Resident with a 3-Day Notice
-          to Pay Rent or Quit on two (2) or more occasions, or Resident has tendered an NSF check on two (2) or monthlyRent
-          occasions, Landlord may thereafter require Resident that all future rental payments be made in the form
-          of a cashier's check or money order only.
-          The rent for a partial month's period shall be prorated on the basis of a 30-day month and shall be paid on
-          or before the next rental due date.
-          Landlord may apply any payment received from Resident, including a payment recevied after the service of a 3-Day Notice
-          to Pay Rent or Quit, in any manner in which the Landlord deems appropriate, including applying such payment first
-          to any unpaid late charges or other fees or charges then due and owing under this agreement, and second, applying the appropriate
-          remainder of the payment to the rent then due.
-      </Label>
       <div className="fields">
-          <div className="three wide field">
+          <div className="sixteen wide field">
+            <label>
+              Resident shall pay to Landlord the Security Deposit set forth in the Basic Lease Terms of this Agreement to SecurityDeposit
+              Resident's compliance with all of the conditions of this Agreement and Landlord's rules and regulations. Unless otherwise
+              requires by law, Resident is not entitled to any interest on the security deposit. No portion of the deposit shall be deemed
+              rent for any rental month, nor shall it constitute a measure of Landlord's damages in the event of a default. Resident agrees
+              that no portion of the deposit shall be a reduction of or offset against rent or any other payment due Landlord.  Within 
+              twenty-one (21) daysafter Resident vacates the premises, the deposit shall be returned by mail to Resident at Resident's last known 
+              address, less the amount of any unpaid rent, the amount reasonably necessary to compensate Landlord for cleaning and repair of 
+              damages to the premises to meet Landlord's re-rental standards, over and above normal wear and tear, and any other amounts due
+              Landlord.  The refund of the deposit shall be accompanied with an itemized statement showing the total amount of the deposit and 
+              the deductions therefrom, if any. If the deposit is insufficient to cover said amounts, Resident shall promptly pay the difference 
+              to Landlord.
+            </label>
+            
             <div>
-              <Label htmlFor="ackPossessionOfPremises">Click to Acknowledge</Label>
+              <Label htmlFor="ackSecurityDeposit">Click to Acknowledge</Label>
               <div>
-                <Field name="ackPossessionOfPremises" id="ackPossessionOfPremises" component="input" type="checkbox"/>
+                <Field name="ackSecurityDeposit" id="ackSecurityDeposit" component="input" type="checkbox"/>
               </div>
             </div>
           </div>
